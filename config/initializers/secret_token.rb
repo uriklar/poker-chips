187335ed9a63b48c5a9f4bb5b3d541a33a4a0e10
uriklar <uriklar@gmail.com>
2014-04-29 18:26:51 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokerChips::Application.config.secret_token = '6e902daae99e80eeb218e29bbd7ea153b138e73bfc980aab62108e84ffa3c3ae0cfe94d1c5896f488d56955015217aa2462db29959899dd9dcf7e024081f416a'
